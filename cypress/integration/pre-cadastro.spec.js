08cddef5d032117fd7ec1405c774aa1f1ebc957f
gscode <adm.guilhermedesousa@gmail.com>
2022-05-12 00:41:42 -0300

    aula 4: refatorando código e criando funcionalidade página de produtos

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -9,13 +9,21 @@ describe('Funcionalidade Pré Cadastro', () => {
   })
 
   it('Deve completar o pré cadastro com sucesso', () => {
-    cy.get('#reg_email').type(faker.internet.email())
+
+    let nomeFaker = faker.name.firstName()
+    let sobrenomeFaker = faker.name.lastName()
+    let emailFaker = faker.internet.email(nomeFaker) // Aqui o email receberá o primeiro nome do usuário
+
+    cy.get('#reg_email').type(emailFaker)
+    // cy.get('#reg_email').type(faker.internet.email()) // Refatorando código
     cy.get('#reg_password').type('teste@teste')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nomeFaker)
+    // cy.get('#account_first_name').type(faker.name.firstName()) // Refatorando código
+    cy.get('#account_last_name').type(sobrenomeFaker)
+    // cy.get('#account_last_name').type(faker.name.lastName()) // Refatorando código
     cy.get('.woocommerce-Button').click()
 
     cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
